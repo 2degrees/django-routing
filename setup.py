aff174bc30a29739393bb144392e9db2449530cb
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2013-08-01 14:39:45 +0100

    Added setup.py, VERSION.txt and empty package

---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2013, 2degrees Limited.
+# All Rights Reserved.
+#
+# This file is part of django-routing
+# <https://github.com/2degrees/django-routing/>, which is subject to the
+# provisions of the BSD at
+# <http://dev.2degreesnetwork.com/p/2degrees-license.html>. A copy of the
+# license should accompany this distribution. THIS SOFTWARE IS PROVIDED "AS IS"
+# AND ANY AND ALL EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT
+# NOT LIMITED TO, THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST
+# INFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+version = open(os.path.join(here, 'VERSION.txt')).readline().rstrip()
+
+
+setup(
+    name='django-routing',
+    version=version,
+    description='',
+    long_description=README,
+    classifiers=[],
+    keywords='',
+    author='2degrees Limited',
+    author_email='2degrees-floss@googlegroups.com',
+    url='http://packages.python.org/django-routing/',
+    license='BSD (http://dev.2degreesnetwork.com/p/2degrees-license.html)',
+    packages=find_packages(exclude=['tests']),
+    tests_require=['coverage', 'nose'],
+    install_requires=[],
+    test_suite='nose.collector',
+    )
